3809305a44da586e316d5f528ec3ee52d4083fc4
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2023-01-18 16:24:19 +0100

    ha

--- sketch.js
+++ sketch.js
@@ -42,7 +42,61 @@ function draw() {
     loadPixels();
     for (let x = 0; x < width; x++) {
       for (let y = 0; y < height; y++) {
-        set(x, y, pickColor(x, y));
+        let index = (x + y * width) * 4;
+        let noiseValue = noise(
+          x * noiseScale + frameCount / 50,
+          y * noiseScale
+        );
+
+        if (noiseValue < 0.57) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.6) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.63) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.66) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.69) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.72) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.75) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.78) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.81) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.84) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else if (noiseValue < 0.87) {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        } else {
+          pixels[index + 0] = 0;
+          pixels[index + 1] = 0;
+          pixels[index + 2] = 0;
+        }
       }
     }
     updatePixels();
@@ -52,40 +106,6 @@ function draw() {
   } else {
     noLoop();
   }
-}
-
-function pickColor(x, y) {
-  let noiseValue = noise(x * noiseScale + frameCount / 50, y * noiseScale);
-  if (noiseValue < 0.57) {
-    c = palette[11];
-  } else if (noiseValue < 0.6) {
-    if (random() > pow(noiseValue - 0.57, 2) * 100) {
-      c = palette[11];
-    } else {
-      c = palette[10];
-    }
-  } else if (noiseValue < 0.63) {
-    c = palette[9];
-  } else if (noiseValue < 0.66) {
-    c = palette[8];
-  } else if (noiseValue < 0.69) {
-    c = palette[7];
-  } else if (noiseValue < 0.72) {
-    c = palette[6];
-  } else if (noiseValue < 0.75) {
-    c = palette[5];
-  } else if (noiseValue < 0.78) {
-    c = palette[4];
-  } else if (noiseValue < 0.81) {
-    c = palette[3];
-  } else if (noiseValue < 0.84) {
-    c = palette[2];
-  } else if (noiseValue < 0.87) {
-    c = palette[1];
-  } else {
-    c = palette[0];
-  }
-  return color(c);
 }
 
 // function windowResized() {
